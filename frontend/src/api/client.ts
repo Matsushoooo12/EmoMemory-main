066b2c60f408a6f8416ebbc1a3575590e2e124c3
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-05-17 09:05:26 +0900

    環境変数してい

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,5 +1,5 @@
-import applyCaseMiddleware from "axios-case-converter";
-import axios from "axios";
+import applyCaseMiddleware from 'axios-case-converter';
+import axios from 'axios';
 
 const options = {
   ignoreHeaders: true,
@@ -7,7 +7,7 @@ const options = {
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: `${process.env.REACT_APP_API_URL}/api/v1`,
   }),
   options
 );
